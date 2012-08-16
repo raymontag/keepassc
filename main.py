f7cd3dde527da7d5b76fdd136a7578d4b2f25c0e
Karsten-Kai König <kkoenig@posteo.de>
2012-08-16 19:44:40 +0200

    implemented db browsing

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from curses.ascii import NL
 from os import *
 from os.path import isdir
 from socket import gethostname
-from sys import exit
+from sys import exit, argv
 
 from kppy import *
 
@@ -37,8 +37,9 @@ class App(object):
         init_pair(1, 7, 0)
         init_pair(2, 2, 0)
         init_pair(3, 0, 1)
-        init_pair(4, 4, 0)
-        init_pair(5, 0, 4)
+        init_pair(4, 6, 0)
+        init_pair(5, 0, 6)
+        init_pair(6, 0, 7)
         self.stdscr.bkgd(1)
 
         self.loginname = getlogin()
@@ -46,13 +47,29 @@ class App(object):
         self.cur_dir = getcwd()
         chdir('/var/empty')
 
-        self.stdscr.addstr(0,0, self.loginname+'@'+self.hostname+':', color_pair(2))
-        self.stdscr.addstr(0, len(self.loginname+'@'+self.hostname+':'), self.cur_dir)
+        self.stdscr.addstr(0,0, self.loginname+'@'+self.hostname+':', 
+                           color_pair(2))
+        self.stdscr.addstr(0, len(self.loginname+'@'+self.hostname+':'), 
+                           self.cur_dir)
 
-        self.main_loop()
+        self.term_size = self.stdscr.getmaxyx()
+        self.db = None
 
     def main_loop(self):
-        self.open_db()
+        while True:
+            self.stdscr.addstr(1,0, 'To open an existing database type \'o\',')
+            self.stdscr.addstr(2,0, 'to create a new one type \'n\'.')
+            self.stdscr.addstr(4,0, 'Type \'q\' to quit.')
+            self.stdscr.refresh()
+
+            c = self.stdscr.getch()
+            if c == ord('o'):
+                self.open_db()
+                self.db_browser()
+            elif c == ord('n'):
+                pass
+            elif c == ord('q'):
+                self.close()
 
     def open_db(self):
         while True:
@@ -62,11 +79,13 @@ class App(object):
                 continue
             else:
                 self.stdscr.erase()
-                self.stdscr.addstr(0,0, self.loginname+'@'+self.hostname+':', color_pair(2))
+                self.stdscr.addstr(0,0, self.loginname+'@'+self.hostname+':', 
+                                   color_pair(2))
                 self.stdscr.addstr(0, len(self.loginname+'@'+self.hostname+':'), 
                                    self.cur_dir)
                 self.stdscr.addstr(1,0, 'Password: ')
                 self.stdscr.refresh()
+
                 c = ''
                 password = ''
                 while c != NL:
@@ -75,11 +94,14 @@ class App(object):
                         continue
                     password += chr(c)
                 
+                password = password[:-1]
+
                 try:
                     self.db = KPDB(filepath, password)
                 except KPError as err:
                     self.stdscr.erase()
-                    self.stdscr.addstr(0,0, self.loginname+'@'+self.hostname+':', color_pair(2))
+                    self.stdscr.addstr(0,0, self.loginname+'@'+self.hostname+':', 
+                                       color_pair(2))
                     self.stdscr.addstr(0, len(self.loginname+'@'+self.hostname+':'), 
                                        self.cur_dir)
                     self.stdscr.addstr(1,0, err.__str__())
@@ -91,15 +113,18 @@ class App(object):
 
     def browser(self):
         self.stdscr.erase()
-        self.stdscr.addstr(0,0, self.loginname+'@'+self.hostname+':', color_pair(2))
+        self.stdscr.addstr(0,0, self.loginname+'@'+self.hostname+':',  
+                           color_pair(2))
         self.stdscr.addstr(0, len(self.loginname+'@'+self.hostname+':'), 
                            self.cur_dir)
         self.stdscr.refresh()
+
         try:
             dir_cont = listdir(self.cur_dir)
         except OSError:
             self.stdscr.erase()
-            self.stdscr.addstr(0,0, self.loginname+'@'+self.hostname+':', color_pair(2))
+            self.stdscr.addstr(0,0, self.loginname+'@'+self.hostname+':', 
+                               color_pair(2))
             self.stdscr.addstr(0, len(self.loginname+'@'+self.hostname+':'), 
                                self.cur_dir)
             self.stdscr.addstr(1,0, 'Was not able to read directory.')
@@ -112,7 +137,6 @@ class App(object):
             return False
             
         rem = []
-
         for i in dir_cont:
             if (not isdir(self.cur_dir+'/'+i) and not i[-3:] == 'kdb') \
                 or i[0] == '.':
@@ -127,17 +151,15 @@ class App(object):
                 dirs.append(i)
             else:
                 files.append(i)
-
         dirs.sort()
         files.sort()
 
         dir_cont = []
         dir_cont.extend(dirs)
         dir_cont.extend(files)
-
         if not self.cur_dir == '/': dir_cont.insert(0, '..');
-        highlight = 0
 
+        highlight = 0
         self.show_dir(highlight, dir_cont)
 
         while True:
@@ -193,5 +215,123 @@ class App(object):
                     self.stdscr.addstr(i+1, 0, dir_cont[i])
         self.stdscr.refresh()
 
+    def db_browser(self):
+        self.group_win = newwin(self.term_size[0]-1, int(self.term_size[1]/3), 
+                                1, 0)
+        self.entry_win = newwin(self.term_size[0]-1, 2*int(self.term_size[1]/3),
+                                1, int(self.term_size[0]/3)+2)
+        self.group_win.keypad(1)
+        self.entry_win.keypad(1)
+        self.group_win.bkgd(1)
+        self.entry_win.bkgd(1)
+
+        cur_root = self.db._root_group
+        cur_win = 0
+        g_highlight = 0
+        e_highlight = 0
+
+        self.show_groups(g_highlight, cur_root, cur_win)
+        self.show_entries(g_highlight, e_highlight, cur_root, cur_win)
+
+        while True:
+            c = self.group_win.getch()
+
+            if c == ord('e'):
+                self.db.close()
+                self.group_win.erase()
+                self.entry_win.erase()
+                self.group_win.refresh()
+                self.entry_win.refresh()
+                break
+            elif c == ord('q'):
+                self.db.close()
+                self.close()
+            elif c == KEY_DOWN:
+                if cur_win == 0:
+                    if g_highlight >= len(cur_root.children)-1:
+                        continue
+                    g_highlight += 1
+                    self.show_groups(g_highlight, cur_root, cur_win)
+                    e_highlight = 0
+                    self.show_entries(g_highlight, e_highlight, cur_root, cur_win)
+                elif cur_win == 1:
+                    if e_highlight >= len(cur_root.children[g_highlight].entries):
+                        continue
+                    e_highlight += 1
+                    self.show_entries(g_highlight, e_highlight, cur_root, cur_win)
+            elif c == KEY_UP:
+                if cur_win == 0:
+                    if g_highlight <= 0:
+                        continue
+                    g_highlight -= 1
+                    self.show_groups(g_highlight, cur_root, cur_win)
+                    e_highlight = 0
+                    self.show_entries(g_highlight, e_highlight, cur_root, cur_win)
+                elif cur_win == 1:
+                    if e_highlight <= 0:
+                        continue
+                    e_highlight -= 1
+                    self.show_entries(g_highlight, e_highlight, cur_root, cur_win)
+            elif c == KEY_LEFT:
+                cur_win = 0
+                self.show_groups(g_highlight, cur_root, cur_win)
+                self.show_entries(g_highlight, e_highlight, cur_root, cur_win)
+            elif c == KEY_RIGHT:
+                cur_win = 1
+                self.show_groups(g_highlight, cur_root, cur_win)
+                self.show_entries(g_highlight, e_highlight, cur_root, cur_win)
+
+    def show_groups(self, highlight, root, cur_win):
+        self.group_win.erase()
+        groups = root.children
+        if cur_win == 0:
+            h_color = 5
+            n_color = 4
+        else:
+            h_color = 6
+            n_color = 1
+        for i in range(len(groups)):
+            if highlight == i:
+                if groups[i].children:
+                    title = '+'+groups[i].title
+                else:
+                    title = ' '+groups[i].title
+                self.group_win.addstr(i, 0, title, 
+                                      color_pair(h_color))
+            else:
+                if groups[i].children:
+                    title = '+'+groups[i].title
+                else:
+                    title = ' '+groups[i].title
+                self.group_win.addstr(i, 0, title,
+                                      color_pair(n_color))
+        self.group_win.refresh()
+
+    def show_entries(self, g_highlight, e_highlight, root, cur_win):
+        self.entry_win.erase()
+        entries = root.children[g_highlight].entries
+        if cur_win == 1:
+            h_color = 5
+            n_color = 4
+        else:
+            h_color = 6
+            n_color = 1
+        for i in range(len(entries)):
+            if e_highlight == i:
+                self.entry_win.addstr(i, 0, entries[i].title,
+                                      color_pair(h_color))
+            else:
+                self.entry_win.addstr(i, 0, entries[i].title,
+                                      color_pair(n_color))
+        self.entry_win.refresh()
+        
 if __name__ == '__main__':
-    app = App()
+    if len(argv) > 1:
+        if argv[1] == '--help' or argv[1] == '-h':
+            pass
+        else:
+            print('Type \'xxx -h\'')
+    else:
+        app = App()
+        app.main_loop()
+
