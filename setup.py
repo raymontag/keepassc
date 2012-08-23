b07068d7d9bf102cc0d3eba68fbb632f661da267
Karsten-Kai König <kkoenig@posteo.de>
2012-08-23 18:59:26 +0200

    bugs...

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup( 
+     name = "keepassc", 
+     version = "1.0", 
+     author = "Karsten-Kai König", 
+     author_email = "kkoenig@posteo.de",
+     url = "",
+     download_url = "",
+     description = "A password manager that is fully compatible to KeePass v.1.x and KeePassX",
+     scripts = ['keepassc'],
+     license = "GPL v3 or later"
+     )
