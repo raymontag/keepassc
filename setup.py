64ed8074e62be65c3af986e552ac09401484521f
Karsten-Kai König <kkoenig@posteo.de>
2012-08-30 14:11:15 +0200

    stable release

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
+'''
+Copyright (C) 2012 Karsten-Kai König <kkoenig@posteo.de>
+
+This file is part of keepassc.
+
+keepassc is free software: you can redistribute it and/or modify it 
+under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or at your 
+option) any later version.
+
+keepassc is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with keepassc.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+
 from distutils.core import setup
 
 setup( 
@@ -9,5 +29,6 @@ setup(
      download_url = "",
      description = "A password manager that is fully compatible to KeePass v.1.x and KeePassX",
      scripts = ['keepassc'],
+     data_files = ['COPYING'],
      license = "GPL v3 or later"
      )
