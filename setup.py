4c7d71e96d6f212a3e96c5fe29fececa39af1991
Karsten-Kai König <kkoenig@posteo.de>
2013-08-21 15:35:36 +0200

    new version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with keepassc.  If not, see <http://www.gnu.org/licenses/>.
 from distutils.core import setup
 
 setup(name = "keepassc",
-      version = "1.6.1",
+      version = "1.6.2",
       author = "Karsten-Kai König, Scott Hansen",
       author_email = "kkoenig@posteo.de",
       url = "http://raymontag.github.com/keepassc",
